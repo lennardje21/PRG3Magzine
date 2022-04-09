82ce9c2681b15feb2a10b4649d3b5591a941255a
Lennard Touw <lennard.touw@gmail.com>
2022-04-09 16:54:18 +0200

    alleen nog addeventlisteners wijzigen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', init);
-window.localStorage;
 
 //global vars
 let fetchLocation = 'webservice/index.php';
@@ -23,35 +22,47 @@ const ajaxRequest = (url, func) => {
 
 
 function createTheThing(data) {
+    //Haal de div spellsGoHere op, deze bestaat al in de index.html
     let spellInfo = document.getElementById('spellsGoHere');
+
+    //spell of data haalt 1 spell uit de json waarin al de spells staan
     for (let spell of data) {
+        //creëer een div en geef die de class spellInfo
         let div = document.createElement("div");
         div.classList.add("spellInfo");
-        div.dataset.name = spell.spellName;
 
+        //Maak een div aan waarin de naam van de spell komt te staan, krijgt class spellName
         let classNameDiv = document.createElement("div")
         classNameDiv.classList.add("spellName");
+
+        //Controleer of de spell een favoriet is, als dat zo is krijgt spellName de class favSpell toegevoegd, dit maakt de tekst goud
         if (spell.spellName === localStorage.getItem(`favorite${spell.id}`)){
             classNameDiv.classList.add("favSpell");
         } else {
             classNameDiv.classList.remove("favSpell");
         }
 
+        //Maak een h2 waarin de naam van de spell wordt gezet met innerHTML
         let h2 = document.createElement("h2");
         h2.innerHTML = spell.spellName;
+
         classNameDiv.appendChild(h2);
         div.appendChild(classNameDiv);
 
+        //Haal de afbeelding naam op en zoek die op in de image folder
         let img = document.createElement("img");
         img.src = `images/${spell.spellImage}`;
         div.appendChild(img);
 
-        //button to make something a favorite spell
+        //Knop om spells aan favorieten toe te voegen
         let favoriteButton = document.createElement('button');
         favoriteButton.classList.add("buttonClass");
         favoriteButton.id = "favoriteTest";
         let inStorage;
 
+
+        //Checkt welke tekst in de knop moet komen(toevoegen of verwijderen) aan de hand van localstorage
+        //wordt gebruikt bij het inladen
         if (spell.spellName === localStorage.getItem(`favorite${spell.id}`)){
             favoriteButton.innerHTML = 'Remove from favorites';
             inStorage = true;
@@ -60,6 +71,9 @@ function createTheThing(data) {
             inStorage = false
         }
 
+
+        /*functie om een spell aan favorieten toe te voegen of te verwijderen, zorgt er ook voor dat de kleur van een favorieten spell van kleur veranderd
+         en de de tekst in de favoriteButton veranderd zonder dat er een refresh nodig is*/
         favoriteButton.addEventListener("click", () => {
             if (inStorage === false) {
                 localStorage.setItem(`favorite${spell.id}`, `${spell.spellName}`);
@@ -76,7 +90,7 @@ function createTheThing(data) {
 
         div.appendChild(favoriteButton);
 
-        //button to show the details of a spell
+        //Maak een knop die je de details van een spell laat zien
         let detailsButton = document.createElement("button");
         detailsButton.classList.add("buttonClass");
         detailsButton.innerHTML = "Show details";
@@ -92,11 +106,16 @@ function createTheThing(data) {
     }
 }
 
+/**
+ *
+ * @param spell
+ */
 
 function showDetails(spell) {
     //Haal de div op waar de data in moet komen
     let spellDetails = document.getElementById("spellDetailsGoHere");
-    //Leegt de div zodat hij niet vol loopt
+
+    //Leeg de div zodat hij niet vol loopt
     spellDetails.innerHTML = '';
     spellDetails.style.display = "block";
 
@@ -110,36 +129,12 @@ function showDetails(spell) {
 
     spellDetails.appendChild(spellName);
 
+    /*Functie gemaakt omdat ik drie keer dezelfde code moest schrijven
+    * vraagt om een div naam(voor variable), een tekst die bold moet worden gemaakt, de spell waaruit de data moet komen en welk specifieke waarde er moet worden opgehaald*/
+    spellDetails.appendChild(test("spellType", "Spell Type: ", spell, 'spellType'));
+    spellDetails.appendChild(test("spellBonus", "Spell Bonus: ", spell, 'bonus'));
+    spellDetails.appendChild(test("spellType", "FP Cost: ", spell, 'FPCost'));
 
-    let spellType = document.createElement("div");
-    spellType.innerHTML = '<br><b>Spell Type:</b> ';
-    spellDetails.appendChild(spellType);
-
-    let spellTypeDiv = document.createElement("div");
-    spellTypeDiv.classList.add("textToRight");
-    spellTypeDiv.innerHTML = spell.spellType;
-    spellType.appendChild(spellTypeDiv);
-
-
-    let spellBonus = document.createElement("div");
-    spellBonus.innerHTML = '<b>Bonus:</b> ';
-
-    let spellBonusDiv = document.createElement("div")
-    spellBonusDiv.classList.add("textToRight")
-    spellBonusDiv.innerHTML = spell.bonus;
-    spellBonus.appendChild(spellBonusDiv);
-
-    spellDetails.appendChild(spellBonus);
-
-    let FPCost = document.createElement("div");
-    FPCost.innerHTML = '<b>FP Cost:</b> ';
-
-    let FPCostDiv = document.createElement("div");
-    FPCostDiv.classList.add("textToRight");
-    FPCostDiv.innerHTML = spell.FPCost;
-    FPCost.appendChild(FPCostDiv);
-
-    spellDetails.appendChild(FPCost);
 
     //haalt de description op
     let descriptionDiv = document.createElement("div");
@@ -147,7 +142,7 @@ function showDetails(spell) {
     spellDetails.appendChild(descriptionDiv);
 
 
-    //knop om het details veld af te sluiten
+    //knop om het details veld af te sluiten/verbergen
     let closeDetails = document.createElement("button")
     closeDetails.classList.add("buttonClass");
     closeDetails.addEventListener("click", () => {
@@ -155,30 +150,22 @@ function showDetails(spell) {
     });
     closeDetails.innerHTML = "close details";
     spellDetails.appendChild(closeDetails);
-
 }
-/*
-function favorite(data){
-    let clickedItem = data.target;
-    console.log(clickedItem);
-    if (clickedItem.nodeName !== "BUTTON") {
-        return;
-    }
 
-    if (inStorage === false) {
-        localStorage.setItem(`favorite${data.id}`, `${data.spellName}`);
-        favoriteButton.innerHTML = "Remove from favorites";
-        classNameDiv.classList.add("favSpell");
-        inStorage = true;
-    } else {
-        localStorage.removeItem(`favorite${data.id}`)
-        favoriteButton.innerHTML = "Add to favorites";
-        classNameDiv.classList.remove("favSpell");
-        inStorage = false;
-    }
 
+function test(divName, item, spell, specification) {
+    divName = document.createElement("div");
+    divName.innerHTML = `<b>${item}</b>`;
+
+    window[divName + "Div"] = document.createElement("div");
+    window[divName + "Div"].classList.add("textToRight");
+    window[divName + "Div"].innerHTML = spell[specification];
+    divName.appendChild(window[divName + "Div"]);
+    return divName;
 }
-*/
+
+
+
 function ajaxErrorHandler(data){
     console.log(data);
 }
